e81fc05c891b6f8925faef6a752470fb120019cc
ligvake <tigidikovic@gmail.com>
2023-02-01 09:55:30 +0300

    Sum inverse added

--- SteelHeartTestConsoleApp/Program.cs
+++ SteelHeartTestConsoleApp/Program.cs
@@ -8,13 +8,21 @@ namespace SteelHeartTestConsoleApp
         static void Main(string[] args)
         {
             int sum = 0;
-            var collection = new List<float> { 5, 325.1f, -7, 46.8f, 1.000f, 98, -999999.1f, 34.7f, 12345, 4, 9, 0.00f, 0, -8.5f };
+            List<float> collection = new List<float> { 5, 325.1f, -7, 46.8f, 1.000f, 98, -999999.1f, 34.7f, 12345, 4, 9, 0.00f, 0, -8.5f };
 
             // Целые числа: 5, -7, 1 (1.000 я взял как целое число), 98, 12345, 4, 9, 0, 0
             // Сумма должна быть равна 12455
 
             // Сортировка с помощью Quick Sort
             QuickSort(collection, 0, collection.Count - 1);
+
+            Console.WriteLine("Отсортированные элементы коллекции: ");
+            foreach (float f in collection)
+            {
+                Console.Write($"{ f }   ");
+            }
+            Console.WriteLine("\n"); // отделяем выводы
+
 
             // Сумма всех целых элементов коллекции
             foreach (float f in collection) {
@@ -23,14 +31,31 @@ namespace SteelHeartTestConsoleApp
                     sum += (int)f;
                 }
             }
+            Console.WriteLine($"Сумма целых элементов коллекции равна: { sum }\n");
 
-            Console.WriteLine("Отсортированные элементы коллекции: ");
-            foreach (float f in collection)
+
+            // Зеркалим сумму
+            List<int> sumArray = new List<int>();
+            int s = 0;
+
+            for(int i = 10; sum != 0; i *= 10)
             {
-                Console.Write($"{ f }   ");
+                // берем число из разряда и добавляем в лист
+                s = (sum % i);
+                sumArray.Add(s / (i / 10));
+                // вычитаем из суммы добавленное число
+                sum -= s;
             }
 
-            Console.WriteLine($"\n \nСумма целых элементов коллекции равна: { sum }");
+            int reversedSum = 0;
+            int digitsCount = sumArray.Count; // кол-во разрядов в числе
+            for (int i = 0; i < digitsCount; i++)
+            {
+                // присваиваем значение элемента, умноженного на инвертированный разряд
+                reversedSum += (int)(sumArray[i] * Math.Pow(10, digitsCount - i - 1));
+            }
+
+            Console.WriteLine($"Инвертированная сумма целых элементов коллекции: { reversedSum }");
             Console.ReadKey();
 
 
